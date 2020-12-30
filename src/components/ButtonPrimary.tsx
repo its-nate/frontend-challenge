81bb44eeab0c719734d1fef12623f465b3a4cde0
Nate Micinski <nmicinski@gmail.com>
2020-12-30 12:21:53 -0800

    fix button link

--- src/components/ButtonPrimary.tsx
+++ src/components/ButtonPrimary.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const ButtonPrimary = (props) => {
     return (
-      <button className="font-content text-white text-xl md:text-2xl rounded-full bg-pink hover:bg-pinkDark p-5">
-        <a href={props.slug}>{props.cta}</a>
-      </button>
+      <a href={props.slug}>
+        <button className="font-content text-white text-xl md:text-2xl rounded-full bg-pink hover:bg-pinkDark p-5">
+          {props.cta}
+        </button>
+      </a>
     );
 }
 
