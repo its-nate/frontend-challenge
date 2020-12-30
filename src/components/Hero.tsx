934a3e22c7d8984cbdfa9f34e3189c333ae93666
Nate Micinski <nmicinski@gmail.com>
2020-12-29 18:55:35 -0800

    moves state management to page instead of component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,51 +2,22 @@ import React from "react";
 import Image from "next/image";
 import ButtonPrimary from "../components/ButtonPrimary";
 
-class Hero extends React.Component<{}, { data: any; loaded: boolean }> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {},
-      loaded: false,
-    };
-  }
+const Hero = (props) => {
+  let { header, content, cta, src } = props.data;
 
-  getData = () => {
-    return fetch("http://localhost:3000/api/cms");
-  };
-
-  async componentDidMount() {
-    await this.getData()
-      .then((res) => res.json())
-      .then((data) =>
-        this.setState({
-          data: data,
-          loaded: true,
-        })
-      );
-  }
-
-  content() {
-    let { header, content, cta, src } = this.state.data;
-
-    return (
-      <div className="flex">
-        <div>
-          <h2 className="text-red-500">{header}</h2>
-          <h4>{content}</h4>
-          <ButtonPrimary slug={"/slug"} cta={cta} />
-          <p>{src}</p>
-        </div>
-        <div className="">
-          <Image src={src} width="792" height="373" />
-        </div>
+  return (
+    <div className="flex">
+      <div>
+        <h2 className="text-red-500">{header}</h2>
+        <h4>{content}</h4>
+        <ButtonPrimary slug={"/slug"} cta={cta} />
+        <p>{src}</p>
       </div>
-    );
-  }
-
-  render() {
-    return <>{this.state.loaded ? this.content() : null}</>;
-  }
-}
+      <div className="">
+        <Image src={src} width="792" height="373" />
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React from "react";
 import Hero from "../components/Hero";
 
-export default function Home() {
+class Home extends React.Component<{}, { data: any; loaded: boolean }> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+      loaded: false,
+    };
+  }
+
+  getData = () => {
+    return fetch("http://localhost:3000/api/cms");
+  };
+
+  async componentDidMount() {
+    await this.getData()
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          data: data,
+          loaded: true,
+        })
+      );
+  }
+
+  content() {
     return (
-        <div className="container flex items-center mx-auto justify-center">
-            <Hero src="/images/hero-example.png" />
-        </div>
-    )
+      <div className="container flex items-center mx-auto justify-center">
+        <Hero data={this.state.data} />
+      </div>
+    );
+  }
+
+  render() {
+    return <>{this.state.loaded ? this.content() : null}</>;
+  }
 }
+
+export default Home;
